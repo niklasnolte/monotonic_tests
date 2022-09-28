379375c92715ae820a233d55eef8be7959f464be
nnolte <niklas.nolte@cern.ch>
2022-09-28 18:47:50 -0400

    compas final

--- compas.py
+++ compas.py
@@ -1,29 +1,32 @@
 import torch
-import torch.nn as nn
-from loaders.compas_loader import load_data
+from loaders.compas_loader import load_data, mono_list
 import torch.utils.data as Data
-from monotonenorm import SigmaNet, direct_norm, GroupSort
+from monotone_utils import SigmaNet, direct_norm, GroupSort
 from sklearn.metrics import accuracy_score
 from tqdm import tqdm
 import numpy as np
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
 
 X_train, y_train, X_test, y_test = load_data(get_categorical_info=False)
 
-feature_num = X_train.shape[1]
-mono_feature = 4
 X_train = torch.tensor(X_train).float().to(device)
 X_test = torch.tensor(X_test).float().to(device)
 y_train = torch.tensor(y_train).float().unsqueeze(1).to(device)
 y_test = torch.tensor(y_test).float().unsqueeze(1).to(device)
+
+mean = X_train.mean(0)
+std = X_train.std(0)
+X_train = (X_train - mean) / std
+X_test = (X_test - mean) / std
+
+monotone_constraints = [1 if i in mono_list else 0 for i in range(X_train.shape[1])]
+
 data_train = Data.TensorDataset(X_train, y_train)
 
-print(X_train.shape)
+criterion = torch.nn.BCEWithLogitsLoss()
 
-criterion = nn.BCEWithLogitsLoss()
-
-per_layer_lip = 4
+per_layer_lip = 2
 
 
 def run(seed):
@@ -33,7 +36,7 @@ def run(seed):
   width = 2
   
   network = torch.nn.Sequential(
-    direct_norm(torch.nn.Linear(13, width), kind="one-inf", max_norm=per_layer_lip),
+    direct_norm(torch.nn.Linear(X_train.shape[1], width), kind="one-inf", max_norm=per_layer_lip),
     GroupSort(width//2),
     direct_norm(torch.nn.Linear(width, width), kind="inf", max_norm=per_layer_lip),
     GroupSort(width//2),
@@ -41,21 +44,20 @@ def run(seed):
     GroupSort(width//2),
     direct_norm(torch.nn.Linear(width, 1), kind="inf", max_norm=per_layer_lip),
   )
-  network = SigmaNet(network, sigma=per_layer_lip**4, monotone_constraints=[1]*mono_feature + [0]*(13-mono_feature))
+  network = SigmaNet(network, sigma=per_layer_lip**4, monotone_constraints=monotone_constraints)
   
   network = network.to(device) 
 
-  nparams = sum(p.numel() for p in network.parameters() if p.requires_grad)
-  print('total param amount:', nparams)
+  print("params:", sum(p.numel() for p in network.parameters()))
 
-  optimizer = torch.optim.Adam(network.parameters(), lr=1e-3)
+  optimizer = torch.optim.Adam(network.parameters(), lr=3e-4)
   
   data_train_loader = Data.DataLoader(
       dataset=data_train,      
       batch_size=256,
       shuffle=True,
   )
-  bar = tqdm(range(1000))
+  bar = tqdm(range(2000))
   for i in bar:
     for X,y in data_train_loader:
       y_pred = network(X)
@@ -64,17 +66,16 @@ def run(seed):
       loss_train.backward()
       optimizer.step()
     
-    #test
-    y_pred = network(X_test)
-    loss = criterion(y_pred, y_test)
-    # accuracy
-    acci = 0
-    for i in torch.linspace(0,1,50):
-      acci = max(acci, accuracy_score(y_test.cpu().detach().numpy(), (y_pred.cpu().detach().numpy() > i.item()).astype(int)))
-    
-    acc = max(acc, acci)
-    bar.set_description(f"train: {loss_train.item():.4f}, test: {loss.item():.4f}, current acc: {acci:.4f}, best acc: {acc:.4f}")
+    with torch.no_grad():
+      y_pred = network(X_test)
+      loss = criterion(y_pred, y_test)
+      acci = 0
+      for i in torch.linspace(0,1,50):
+        acci = max(acci, accuracy_score(y_test.cpu().detach().numpy(), (y_pred.cpu().detach().numpy() > i.item()).astype(int)))
+      
+      acc = max(acc, acci)
+      bar.set_description(f"train: {loss_train.item():.4f}, test: {loss.item():.4f}, current acc: {acci:.4f}, best acc: {acc:.4f}")
   return acc  
 
 accs = [run(i) for i in range(3)]
-print(f"mean: {np.mean(accs)}, std: {np.std(accs)}")
+print(f"mean: {np.mean(accs):.4f}, std: {np.std(accs):.4f}")
