dcb08f0adf52327fcc922208c6fbc081ff3ecf0e
nnolte <niklas.nolte@cern.ch>
2023-02-10 16:01:27 -0500

    auto mpg

---
+++ auto_mpg.py
@@ -0,0 +1,73 @@
+# %%
+import pandas as pd
+import torch
+import tqdm
+from monotone_utils import GroupSort, direct_norm, SigmaNet
+# %%
+df = pd.read_csv('data/auto-mpg.csv')
+df = df[df.horsepower != "?"]
+# %%
+# mpg is regression target
+# cylinders, displacement, horsepower, weight, acceleration, model year, origin are features
+X = df.drop(columns=['mpg', 'car name']).values
+Y = df['mpg'].values
+X = torch.tensor(X.astype(float), dtype=torch.float32)
+Y = torch.tensor(Y.astype(float), dtype=torch.float32).view(-1, 1)
+X = (X - X.mean(0)) / X.std(0)
+Ymean = Y.mean(0)
+Ystd = Y.std(0)
+Y = (Y - Ymean) / Ystd
+# %%
+rmses = []
+for seed in range(3):
+  torch.manual_seed(seed)
+  # split in train and test
+  randperm = torch.randperm(X.shape[0])
+  X = X[randperm]
+  Y = Y[randperm]
+  split = int(0.8 * X.shape[0])
+  Xtr = X[:split]
+  Ytr = Y[:split]
+  Xts = X[split:]
+  Yts = Y[split:]
+
+  width = 8
+
+  model = torch.nn.Sequential(
+    direct_norm(torch.nn.Linear(X.shape[1], width), kind="one-inf"),
+    GroupSort(width//2),
+    direct_norm(torch.nn.Linear(width, width), kind="inf"),
+    GroupSort(width//2),
+    direct_norm(torch.nn.Linear(width, 1), kind="inf"),
+  )
+  model = SigmaNet(model, sigma=1, monotone_constraints=[0,-1,-1,-1,0,0,0])
+
+  # number of elements
+  print(sum(p.numel() for p in model.parameters()))
+
+
+  optimizer = torch.optim.Adam(model.parameters(), lr=1e-2)
+  epochs = 1000
+
+  rmse = float('inf')
+  bar = tqdm.tqdm(range(epochs))
+  for epoch in bar:
+    batch_size = 64
+    for i in range(0, Xtr.shape[0], batch_size):
+      optimizer.zero_grad()
+      yhat = model(Xtr[i:i+batch_size])
+      loss = torch.nn.functional.mse_loss(yhat, Ytr[i:i+batch_size])
+      loss.backward()
+      optimizer.step()
+    with torch.no_grad():
+      yhat = model(Xts)
+      # unscaled rmse
+      new_rmse = torch.sqrt(torch.mean((yhat * Ystd - Yts * Ystd) ** 2))
+      rmse = min(rmse, new_rmse.item())
+      bar.set_description(f"rmse: {new_rmse:.1f}, best: {rmse:.1f}")
+  rmses.append(rmse)
+
+# print mean and std
+print(f"mean: {torch.tensor(rmses).mean():.5f}, std: {torch.tensor(rmses).std():.5f}")
+
+# %%
